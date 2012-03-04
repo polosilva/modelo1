fc3b7bc57d84283560b89530d05147c94b254385
Polo <polosilva@ubuntu.ubuntu-domain>
2012-03-04 05:12:51 -0300

    1er commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Domicilio::Application.config.secret_token = 'bc41a1c53ad727114d542ced8037bb9e1d7a28c4df91b788fe3b644e95e414537acee0feae94816c6a56f5f8953ef80711b722c912e8af20fc4151641c6f9423'
